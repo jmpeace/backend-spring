c0b94b97fd824bf8ed58afc55758658aa6b9f466
Jose Paz <jmpeace@gmail.com>
2021-12-18 10:24:27 -0400

    Initial commit

---
+++ src/main/java/com/happynation/kittik/KittikApplication.java
@@ -0,0 +1,13 @@
+package com.happynation.kittik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KittikApplication.class, args);
+    }
+
+}
